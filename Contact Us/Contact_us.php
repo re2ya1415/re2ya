f200fd34a006748ae357f074ee39e8d16dd307c0
re2ya <53684578+re2ya1415@users.noreply.github.com>
2019-08-13 09:51:28 +0300

    Create Contact_us.php

---
+++ Contact Us/Contact_us.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" lang="English">
+	<title>Contact Us</title>
+</head>
+<body>
+	<h2>
+		We well so happy to recive your advice to improve website or your truble 
+		on our e-mail  ***@***.*** or you can Just send your bullyshit below :
+	</h2>
+
+</body>
+</html>
